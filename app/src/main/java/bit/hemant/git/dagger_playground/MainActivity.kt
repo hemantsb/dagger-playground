a0f191ec802b227c37b9509c3c5189c4ae453fc3
hemantsb <hemant.bisht@outlook.com>
2022-07-31 15:35:54 +0530

    FIELD INJECTION

--- app/src/main/java/bit/hemant/git/dagger_playground/MainActivity.kt
+++ app/src/main/java/bit/hemant/git/dagger_playground/MainActivity.kt
@@ -2,24 +2,26 @@ package bit.hemant.git.dagger_playground
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.ui.AppBarConfiguration
 import bit.hemant.git.dagger_playground.dagger.Car
 import bit.hemant.git.dagger_playground.dagger.CarComponent
 import bit.hemant.git.dagger_playground.dagger.DaggerCarComponent
 import bit.hemant.git.dagger_playground.databinding.ActivityMainBinding
+import javax.inject.Inject
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
-    private lateinit var car: Car
+    @Inject
+    lateinit var car: Car
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         val carComponent: CarComponent = DaggerCarComponent.create()
-        car = carComponent.getCar()
+//        car = carComponent.getCar()
+        carComponent.injectMain(this)
         car.drive()
     }
 
--- app/src/main/java/bit/hemant/git/dagger_playground/dagger/CarComponent.kt
+++ app/src/main/java/bit/hemant/git/dagger_playground/dagger/CarComponent.kt
@@ -1,8 +1,12 @@
 package bit.hemant.git.dagger_playground.dagger
 
+import bit.hemant.git.dagger_playground.MainActivity
 import dagger.Component
 
 @Component
 interface CarComponent {
     fun getCar(): Car
+
+    //for field injection
+    fun injectMain(mainActivity: MainActivity)
 }
